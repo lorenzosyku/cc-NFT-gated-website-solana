9208b3bcc3a013d5b3ec2ddef8a974fd81e7804c
lorenzosyku <lorenzo.naomi1920@gmail.com>
2023-01-08 11:54:46 -0500

    installed dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThirdwebProvider } from "@thirdweb-dev/react/solana";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+
+const network = "devnet";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider
+      network={network}
+      authConfig={{
+        authUrl: "/api/auth",
+        domain: "example.org",
+        loginRedirect: "/",
+      }}
+    >
+      <WalletModalProvider>
+        <Component {...pageProps} />
+      </WalletModalProvider>
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
